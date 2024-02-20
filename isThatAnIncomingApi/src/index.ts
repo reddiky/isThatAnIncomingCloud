36c28e7c1f15efec8201a80b330769fb3d3216bd
Kyle Redding <kyle@reddiky.com>
2024-02-20 00:19:16 -0600

    cors *

--- isThatAnIncomingApi/src/index.ts
+++ isThatAnIncomingApi/src/index.ts
@@ -111,7 +111,7 @@ await server.start();
 
 app.use(
   '/weather',
-  cors<cors.CorsRequest>({ origin: ['https://isthatanincoming.cloud/weather', 'https://reddiky.github.io', 'https://reddiky.github.io/isThatAnIncomingCloud/location'] }),
+  cors<cors.CorsRequest>(),
   express.json(),
   expressMiddleware(server, {
     context: async ({ req }) => ({ token: req.headers.token }),
