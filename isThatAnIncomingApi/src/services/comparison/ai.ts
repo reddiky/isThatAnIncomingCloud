c930381cdc6692b2e7e1dfddd22262d5eb27bb99
Kyle Redding <kyle@reddiky.com>
2024-02-20 07:34:05 -0600

    trying images again

--- isThatAnIncomingApi/src/services/comparison/ai.ts
+++ isThatAnIncomingApi/src/services/comparison/ai.ts
@@ -6,10 +6,13 @@ const openai = new OpenAI({
   apiKey: secretKey,
 });
 async function compareWeather(loc1, loc2, weather1, weather2, survey) {
+  let content = survey.description.length > 0 ? `I strongly like an average temperature of ${survey.temp} and prefer ${survey.description} days. If its below freezing and raining, that counts as snow. Assume all temperatures are in fahrenheit. Will I like ${loc1.name}: ${JSON.stringify(weather1)} or ${loc2.name}: ${JSON.stringify(weather2)} better and why?` :
+  `I strongly prefer an average temperature of ${survey.temp}. If its below freezing and raining, that counts as snow. Assume all temperatures are in fahrenheit. Will I like ${loc1.name}: ${JSON.stringify(weather1)} or ${loc2.name}: ${JSON.stringify(weather2)} better and why?`
+  
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",   
     messages: [
-      { role: "user", content: `I strongly like an average temperature of ${survey.temp} and prefer ${survey.description} days. If its below freezing and raining, that counts as snow. Assume all temperatures are in fahrenheit. Will I like ${loc1.name}: ${JSON.stringify(weather1)} or ${loc2.name}: ${JSON.stringify(weather2)} better and why?` },
+      { role: "user", content },
     ],
     temperature: 1,
     max_tokens: 256,
