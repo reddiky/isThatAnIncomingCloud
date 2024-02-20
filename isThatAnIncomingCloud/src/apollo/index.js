d8458824d1e5a91ee6605e8da6d5fd5086509eb4
Kyle Redding <kyle@reddiky.com>
2024-02-20 07:16:16 -0600

    still cors

--- isThatAnIncomingCloud/src/apollo/index.js
+++ isThatAnIncomingCloud/src/apollo/index.js
@@ -2,17 +2,17 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core
 import { createApolloProvider } from '@vue/apollo-option'
 
 // HTTP connection to the API
-const httpLink = createHttpLink({
-  // You should use an absolute URL here
-  uri: 'https://www.isthatanincoming.cloud/weather',
-})
+// const httpLink = createHttpLink({
+//   // You should use an absolute URL here
+//   uri: 'https://www.isthatanincoming.cloud/weather',
+// })
 
 // Cache implementation
 const cache = new InMemoryCache()
 
 // Create the apollo client
 const apolloClient = new ApolloClient({
-  link: httpLink,
+  uri: 'https://www.isthatanincoming.cloud/weather',
   cache,
 })
 
