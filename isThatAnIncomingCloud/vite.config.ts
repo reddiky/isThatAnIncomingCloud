0a215cd0dc9ace14c4890deb5cfdadccc2cef4c5
Kyle Redding <kyle@reddiky.com>
2024-02-19 21:48:33 -0600

    ?

--- isThatAnIncomingCloud/vite.config.ts
+++ isThatAnIncomingCloud/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
-  base: "isThatAnIncomingCloud/",
+  base: "/isThatAnIncomingCloud/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
